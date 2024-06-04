11166ea8a74fa1b67f36439951ffa09ca76fc7bd
yen5004 <119946331+yen5004@users.noreply.github.com>
2024-06-04 10:39:47 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,6 @@
-!#?bin/bash
+!#/bin/bash
 #Helper script to assist in loading of github repps and seeting up kits
+
 
 
 #check for "C_lab" folder exisits in home directory and if not creates one
@@ -41,6 +42,8 @@ get_timestamp() {
   date +"%d%b%Y_%H:%M:%S-%p"
 }
 
+echo "Install log created, begin tracking - $(get_timestamp)" >> $logg
+
 logg="$HOME/C_lab/install_log"
 # Open new terminal to monitor install_log
 echo "Opening new terminal for monitoring of intall_log..."
@@ -48,16 +51,19 @@ sleep 4
 gnome-terminal --command="watch -n 2 cat $logg"
 echo "Install log created, begin tracking - $(get_timestamp)" >> $logg
 
+# Update and upgrade machine
+echo "Start machine update & upgrade - $(get_timestamp)" >> $logg
+sudo apt update -y && sudo apt upgrade -y
+echo "Finish machine update & upgrade - $(get_timestamp)" >> $logg
+
 #create gitlab folder for downloads:
 cd $folder
 mkdir gitlabs && echo "Created 'gitlabs' folder - $(get_timestamp)" >> $logg
-cd gitlabs
-echo "cd gitlabs - $(get_timestamp)" >> $logg
+cd gitlabs && echo "cd gitlabs - $(get_timestamp)" >> $logg
 echo "pwd: $PWD - $(get_timestamp)" >> $logg
 
-
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # Download the following gitlab repos:
-
 
 repo_urls=(
 # List of GitLab reps urls:
@@ -100,7 +106,7 @@ repo_urls=(
 # Directory of where repos will be cloned:
 clone_dir="$folder/gitlabs"
 
-for repo_url in "$${}repo_urls[@]}"; do
+for repo_url in "${repo_urls[@]}"; do
   repo_name=$(basename "repo_url" .git) # Extract repo name from url
   echo "Cloning $repo_name from $repo_url..."
   echo "Cloned $repo_name from $repo_url into $clone_dir - $(get_timestamp)" >> $logg
@@ -108,21 +114,45 @@ for repo_url in "$${}repo_urls[@]}"; do
 done
 
 
+# Special instructions for gits:
+
+# Special install for cheat:
+ echo "Clone & install cheat - $(get_timestamp)" >> $logg
+cd /tmp \
+  && wget https://github.com/cheat/cheat/releases/download/4.4.2/cheat-linux-amd64.gz \
+  && gunzip cheat-linux-amd64.gz \
+  && chmod +x cheat-linux-amd64 \
+  && sudo mv cheat-linux-amd64 /usr/local/bin/cheat
+echo "Clone & install cheat complete - $(get_timestamp)" >> $logg
+echo "Setting up cheat for the first time, standby..." && echo "Setting up cheat - $(get_timestamp)" >> $logg
+yes | cheat scp
+echo "cheat set up cheat complete." && echo "cheat set up cheat complete - $(get_timestamp)" >> $logg
 
 
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# Python installs
 
-
-#https://github.com/yen5004/updog.git
-pip3 install updog
-
-#https://github.com/Ciphey/Ciphey.git
+# Start install of Ciphey
+echo "Start python install of Ciphey..." && echo "Start python install of Ciphey - $(get_timestamp)" >> $logg
 python3 -m pip install ciphey --upgrade
+echo "Finish python install of Ciphey..." && echo "Fininsh python install of Ciphey - $(get_timestamp)" >> $logg
+
+# Start install of updog
+echo "Start python install of updog..." && echo "Start python install of updog - $(get_timestamp)" >> $logg
+pip3 install updog
+echo "Finish python install of updog..." && echo "Fininsh python install of updog - $(get_timestamp)" >> $logg
 
 
-https://github.com/cheat/cheat.git
 
 
-apt install flameshot
+
+
+
+
+# apt install section
+cd ~ && && echo "cd ~ - $(get_timestamp)" >> $logg
+echo "pwd: $PWD - $(get_timestamp)" >> $logg
+sudo apt install -y flameshot && echo "sudo apt install -y flameshot - $(get_timestamp)" >> $logg
 
 
 
