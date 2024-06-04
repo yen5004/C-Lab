fc1934191a802175b9348ef78e8330fb9dc366ca
yen5004 <119946331+yen5004@users.noreply.github.com>
2024-06-04 08:39:34 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -48,19 +48,47 @@ echo "Install log created, begin tracking - $(get_timestamp)" >> $logg
 mkdir gitlabs && echo "Created 'gitlabs' folder - $(get_timestamp)" >> $logg
 cd gitlabs
 
+# Download the following gitlab repos:
+
+# List of GitLab reps urls:
+"https://github.com/yen5004/1-liner-ls--la-.git"
+"https://github.com/yen5004/ZIP_TAR.git"
+"https://github.com/yen5004/Encrypt_Decrypt.git"
+"https://github.com/yen5004/Encode_Decode.git"
+"https://github.com/yen5004/EICAR.git"
+"https://github.com/yen5004/THM_BashScripting.git"
+"https://github.com/yen5004/THM_ToolBox-Vim.git"
+"https://github.com/yen5004/THM_AV_Evasion-Shellcode.git"
+"https://github.com/yen5004/Hash_Hash.git"
+"https://github.com/yen5004/SCP_file_sender.git"
+"https://github.com/yen5004/tmux_quad_screen_user_input.git"
+"https://github.com/yen5004/tmux_script.git"
+"https://github.com/nyxgeek/bashscan.git"
+"https://github.com/yen5004/Bash-Oneliner.git"
+"https://github.com/dafthack/SharpUp.git"
+"https://github.com/Hack-the-box/PowerShellMafia.git"
+"https://github.com/dafthack/HostRecon.git"
+"https://github.com/dafthack/RDPSpray.git"
+"https://github.com/dafthack/Misc-Powershell-Scripts.git"
+"https://github.com/frizb/MSF-Venom-Cheatsheet.git"
+"https://github.com/swisskyrepo/PayloadsAllTheThings.git"
+"https://github.com/initstring/uptux.git"
+"https://github.com/Hack-the-box/unicorn.git"
+"https://github.com/andrewjkerr/security-cheatsheets.git"
+"https://github.com/yen5004/DevMal_U.git"
+"https://github.com/yen5004/1-liner-keep-alive.git"
+"https://github.com/0x09AL/RdpThief.git"
 
 
 
+https://github.com/yen5004/updog.git
+pip3 install updog
+
+https://github.com/Ciphey/Ciphey.git
+python3 -m pip install ciphey --upgrade
 
 
-
-
-
-
-
-
-
-
+https://github.com/cheat/cheat.git
 
 
 
